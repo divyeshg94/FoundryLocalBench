e00c3298f2f021123321d922b4bb98bc85f77cbc
Divyesh Govaerdhanan <divyesh.govaerdhanan@plateauinc.com>
2026-01-10 16:34:34 -0500

    Updated Program.cs to show the models

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AI.Foundry.Local;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+var config = new Configuration
+{
+    AppName = "app-name",
+    LogLevel = Microsoft.AI.Foundry.Local.LogLevel.Information,
+};
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+});
+var logger = loggerFactory.CreateLogger<Program>();
+
+await FoundryLocalManager.CreateAsync(config, logger);
+var manager = FoundryLocalManager.Instance;
+
+var catalog = await manager.GetCatalogAsync();
+var models = await catalog.ListModelsAsync();
+
+Console.WriteLine($"Models available: {models.Count()}");
+foreach (var model in models)
+{
+    Console.WriteLine($"- {model.Name} ({model.Id})");
+}
